b7c6e812698bfb49502ac1ac8737091e6c9842c8
ValenDD <valentindutra99@gmail.com>
2024-05-14 21:32:44 -0300

    update index

--- README.md
+++ README.md
@@ -7,3 +7,19 @@ docker-compose up
 
 Cada vez que se realice un cambio hay que ejecutar
 docker-compose up --build
+
+
+Agregar documento a elasticsearch
+http://localhost:9200/my_index/_doc/nro_doc
+
+Consultar mediante campos que usa indices invertidos
+http://localhost:9200/my_index/_search
+luego le tenes que mandar la query de la siguiente forma
+{
+  "query": {
+    "multi_match": {
+      "query": "data science",
+      "fields": ["selftext"]
+    }
+  }
+}
--- backend/app/elastic_search/create_index.py
+++ backend/app/elastic_search/create_index.py
@@ -13,14 +13,19 @@ def create_index():
     index_name = "my_index"
 
     mapping = {
-        "mappings": {
-            "properties": {
-                "field1": {"type": "text"},
-                "field2": {"type": "text"},
-                "field3": {"type": "text"},
-            }
+    "mappings": {
+        "properties": {
+            "title": {"type": "text"},
+            "author": {"type": "text"},
+            "subreddit": {"type": "text"},
+            "created_utc": {"type": "date"},
+            "score": {"type": "integer"},
+            "num_comments": {"type": "integer"},
+            "url": {"type": "text"},
+            "selftext": {"type": "text"}
         }
     }
+}
 
     if not elasticsearch_connection.indices.exists(index=index_name):
         elasticsearch_connection.indices.create(index=index_name, body=mapping)
